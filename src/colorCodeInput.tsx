d0ce8ec41106fc4264c6e3e52eb7e5bfb0b76cc3
Aki Sakata <akisoqls@gmail.com>
2022-02-24 03:00:56 +0900

    Added the ability to save appearance settings in Cookie.

--- src/colorCodeInput.tsx
+++ src/colorCodeInput.tsx
@@ -1,5 +1,6 @@
 import {colorList,appearance} from './types'
-import React from 'react'
+import React, { useEffect } from 'react'
+import {useCookies} from 'react-cookie'
 // import styled from 'styled-components'
 
 type InputProps = {
@@ -7,10 +8,37 @@ type InputProps = {
   stateAction: React.Dispatch<React.SetStateAction<appearance>>;
 }
 
-export const ColorCodeInput: React.VFC<InputProps> = (props:InputProps) => {
+// background: window.matchMedia('(prefers-color-scheme: dark)').matches ? '#000000' :'#eeeeee'
+export const defaultSettings: appearance = {
+  width: 400,
+  height: 4000,
+  colors:{
+    hour: '#e76a2b',
+    minute: '#75d87d',
+    second: '#529ef5',
+    background: '#eeeeee' ,
+    circleBackground: '#eeeeee',
+  },
+  analogClock: true
+} 
+
+export const ColorCodeInput: React.VFC<InputProps> = (props: InputProps) => {
+  const [appearanceFromCookie, setAppearanceCookie, removeAppearanceCookie] = useCookies(['settings'])
+  const settings: appearance = appearanceFromCookie.settings || defaultSettings
+  useEffect(() => {
+    props.stateAction(settings)
+  },[settings])
   return (
     <section>
       <h2>Appearance Settings</h2>
+      <button
+        onClick={() => {
+          removeAppearanceCookie('settings')
+          props.stateAction(defaultSettings)
+        }}
+      >
+        clear
+      </button>
       <ul>
         <li>
           <p>
@@ -19,13 +47,15 @@ export const ColorCodeInput: React.VFC<InputProps> = (props:InputProps) => {
             </label>
             <input
               type={'checkbox'}
-              checked={props.appearance.analogClock}
+              checked={settings.analogClock}
               onChange={
                 (e) => {
-                  props.stateAction({
-                    ...props.appearance,
+                  const newValues :appearance = {
+                    ...settings,
                     analogClock:e.target.checked
-                  })
+                  }
+                  props.stateAction(newValues)
+                  setAppearanceCookie('settings', newValues)
                 }
               }
               id={'analog_clock'}
@@ -33,7 +63,7 @@ export const ColorCodeInput: React.VFC<InputProps> = (props:InputProps) => {
           </p>
         </li>
         {
-          Object.keys(props.appearance.colors).map((v, i) => {
+          Object.keys(settings.colors).map((v, i) => {
             const key = v as keyof colorList
             return (
               key != 'circleBackground'
@@ -44,16 +74,18 @@ export const ColorCodeInput: React.VFC<InputProps> = (props:InputProps) => {
                     </label>
                     <input
                       type={'color'}
-                      value={props.appearance.colors[key]}
+                      value={settings.colors[key]}
                       onChange={
                         (e) => {
-                          props.stateAction({
-                            ...props.appearance,
+                          const newValues: appearance = {
+                            ...settings,
                             colors: {
-                              ...props.appearance.colors,
+                              ...settings.colors,
                               [key]:e.target.value
                             }
-                          })
+                          }
+                          props.stateAction(newValues)
+                          setAppearanceCookie('settings', newValues)
                         }
                       }
                       id={key}
